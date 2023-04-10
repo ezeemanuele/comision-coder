8f4b33e8e1817b35b6cb3dcaae34f4ba28fe939d
Christian Ezequiel Emanuele <ezeemanuele@gmail.com>
2023-04-10 18:42:51 +0200

    agregando java al proyecto

--- java/app.js
+++ java/app.js
@@ -1,4 +1,7 @@
 addEventListener('DOMContentLoaded', () => {
-    const boton-menu = document.querySelector('.boton-menu')
-    if (boton-m)
+    const btn_menu = document.querySelector('.btn-menu')
+    if (btn_menu) {
+        btn_menu.addEventListener('click', () => { 
+        })
+    }
 })
